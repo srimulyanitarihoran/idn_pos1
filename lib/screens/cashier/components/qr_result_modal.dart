c69c75b66d603a8e79e000988a348b88c432072b
srimulyanitarihoran <srimulyanisri449@gmail.com>
2026-01-22 08:54:51 +0700

    finishin qr result modal components

--- lib/screens/cashier/components/qr_result_modal.dart
+++ lib/screens/cashier/components/qr_result_modal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:idn_pos1/utils/currency_format.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrResultModal extends StatefulWidget {
   final String qrData;
@@ -6,7 +8,13 @@ class QrResultModal extends StatefulWidget {
   final bool isPrinting;
   final VoidCallback onClose;
 
-  const QrResultModal({super.key, required this.qrData, required this.total, required this.isPrinting, required this.onClose});
+  const QrResultModal({
+    super.key,
+    required this.qrData,
+    required this.total,
+    required this.isPrinting,
+    required this.onClose,
+  });
 
   @override
   State<QrResultModal> createState() => _QrResultModalState();
@@ -27,7 +35,8 @@ class _QrResultModalState extends State<QrResultModal> {
     if (widget.isPrinting) {
       Future.delayed(Duration(seconds: 2), () {
         // cek jika proses delayed sesuai dengan waktu yang dibutuhkan printer ketika mencetak.
-        if (mounted) { // kondisi ketika widget nya itu aktif, dia akan menjalankan intruksi jika widgetnya aktif, yaiut intruksi if, kalau engga, ga bakal jalan.
+        if (mounted) {
+          // kondisi ketika widget nya itu aktif, dia akan menjalankan intruksi jika widgetnya aktif, yaiut intruksi if, kalau engga, ga bakal jalan.
           setState(() {
             _printFinished = true; // ubah status jadi selesai
           });
@@ -44,7 +53,8 @@ class _QrResultModalState extends State<QrResultModal> {
     IconData statusIcon;
     String statusText;
 
-    if (!widget.isPrinting) { // ! artinya not, berarti artinya tanpa printer.
+    if (!widget.isPrinting) {
+      // ! artinya not, berarti artinya tanpa printer.
       // kondisi 1: printer mati atau mode tanpa printer.
       statusColor = Colors.orange;
       statusBgColor = Colors.orange.shade50;
@@ -64,6 +74,104 @@ class _QrResultModalState extends State<QrResultModal> {
       statusText = 'Cetak Selesai';
     }
 
-    return Container();
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.65,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+      ),
+      padding: EdgeInsets.all(24),
+      child: Column(
+        children: [
+          // handle bar
+          Container(
+            width: 50,
+            height: 5,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // ini adalah buat status mode
+          AnimatedContainer(
+            duration: Duration(
+              milliseconds: 300,
+            ), // untuk memberikan efek animasi halus
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: statusBgColor,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: statusColor),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(statusIcon, size: 20, color: statusColor),
+                SizedBox(width: 10),
+                Text(
+                  statusText,
+                  style: TextStyle(
+                    color: statusColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Text(
+            'SCAN UNTUK MEMBAYAR',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Color(0xFF2E3192),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            'Total: ${formatRupiah(widget.total)}',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+
+          // ini untuk qr code container
+          Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade200),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue.withValues(alpha: 0.1),
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+            child: QrImageView(
+              data: widget.qrData,
+              version: QrVersions.auto,
+              size: 220.0,
+            ),
+          ),
+          Spacer(),
+          // close button
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[200],
+                foregroundColor: Colors.black
+              ),
+              onPressed: widget.onClose,
+              child: Text('Tutup'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
